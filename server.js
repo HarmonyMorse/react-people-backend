184eb3fe710a62d2859005d72486cdfb49bc714a
HarmonyMorse <harmonythedev@gmail.com>
2024-02-02 07:13:07 -0800

    Add people router and middleware

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const express = require("express");
 // Create application object
 const app = express();
 
+// Import people router
+const peopleRouter = require("./routes/people")
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Routes
+app.use("/people", peopleRouter)
+
+
 // Create a test route
 app.get("/", (req, res) => {
     res.send("hello world");
